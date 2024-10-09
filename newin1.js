dd3501cb782da175e644f968e827419eacbe5b89
AthashreeDoiphode <athashreedoiphode@gmail.com>
2024-10-09 09:56:29 +0530

    Update newin1.js

--- newin1.js
+++ newin1.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
-        { img: 'newin/nc1.jpg', name: 'High waist Straight fit Wide leg Cargo jeans', price: 'Rs. 799' },
-        { img: 'newin/nk1.avif', name: 'Peach Colored Chikankaari Short Kurti', price: 'Rs. 1,299' },
-        { img: 'newin/nk2.avif', name: 'Shaded Blue embroidered Kurti', price: 'Rs. 1,299.00' },
-        { img: 'newin/nc2.jpg', name: 'High waist Straight fit Cargo jeans', price: 'Rs. 799.00' },
-        { img: 'newin/no1.avif', name: 'Red Printed Oversized T-shirt', price: 'Rs. 799.00' },
-        { img: 'newin/nt1.jpg', name: 'Brown-peach colored floral Gorgette crop top', price: 'Rs. 999.00' },
-        { img: 'newin/no2.avif', name: 'Pink Calligraphed Oversized T-shirt', price: 'Rs. 1,499.00' },
-        { img: 'newin/nk3.avif', name: 'Red Embroidered Kurti', price: 'Rs. 699.00' },
-        { img: 'newin/nc3.jpg', name: 'High waist Straight fit Cargo jeans', price: 'Rs. 1,899.00' },
-        { img: 'newin/no3.avif', name: 'Light brown Printed Oversized T-shirt', price: 'Rs. 1,899.00' },
-        { img: 'newin/nt2.jpg', name: 'Knitted white crop top', price: 'Rs. 1,899.00' },
-        { img: 'newin/nt3.avif', name: 'Black Polyster crop top', price: 'Rs. 699.00' }
+        { img: 'nc1.jpg', name: 'High waist Straight fit Wide leg Cargo jeans', price: 'Rs. 799' },
+        { img: 'nk1.avif', name: 'Peach Colored Chikankaari Short Kurti', price: 'Rs. 1,299' },
+        { img: 'nk2.avif', name: 'Shaded Blue embroidered Kurti', price: 'Rs. 1,299.00' },
+        { img: 'nc2.jpg', name: 'High waist Straight fit Cargo jeans', price: 'Rs. 799.00' },
+        { img: 'no1.avif', name: 'Red Printed Oversized T-shirt', price: 'Rs. 799.00' },
+        { img: 'nt1.jpg', name: 'Brown-peach colored floral Gorgette crop top', price: 'Rs. 999.00' },
+        { img: 'no2.avif', name: 'Pink Calligraphed Oversized T-shirt', price: 'Rs. 1,499.00' },
+        { img: 'nk3.avif', name: 'Red Embroidered Kurti', price: 'Rs. 699.00' },
+        { img: 'nc3.jpg', name: 'High waist Straight fit Cargo jeans', price: 'Rs. 1,899.00' },
+        { img: 'no3.avif', name: 'Light brown Printed Oversized T-shirt', price: 'Rs. 1,899.00' },
+        { img: 'nt2.jpg', name: 'Knitted white crop top', price: 'Rs. 1,899.00' },
+        { img: 'nt3.avif', name: 'Black Polyster crop top', price: 'Rs. 699.00' }
     ];
 
     const productGrid = document.getElementById('productGrid');
