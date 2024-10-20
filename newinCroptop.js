f6291401d211d65201f31e89a3ba5cf55405bd6b
AthashreeDoiphode <athashreedoiphode@gmail.com>
2024-10-20 13:02:32 +0530

    Update newinCroptop.js

--- newinCroptop.js
+++ newinCroptop.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
-        { img: 'newin/nt1.jpg', name: 'Brown-peach colored floral Gorgette crop top', price: 799 },
-        { img: 'newin/nt2.jpg', name: 'Knitted white crop top', price: 1299},
-        { img: 'newin/nt3.avif', name: 'Black Polyster crop top', price: 1000},
-        { img: 'newin/nt4.avif', name: 'Short ruffle sleeved red crop top', price: 900 },
-        { img: 'newin/nt5.avif', name: 'Long sleeved off-shoulder floral top', price: 800},
-        { img: 'newin/nt6.avif', name: 'Long sleeved black top', price: 600 },
-        { img: 'newin/nt7.avif', name: 'Pink Striped Bell Sleeves Crop Top', price:700 },
-        { img: 'newin/nt8.avif', name: '>Womens Black Tie-Up Waist Crop Top', price:  800},
-        { img: 'newin/nt9.avif', name: 'White Twist Detail Collar Neck top', price: 900},
-        { img: 'newin/nt10.avif', name: 'Multi-Color Floral Print off-Shoulder Bardot Top', price:1000 },
-        { img: 'newin/nt11.avif', name: 'Pink Geometric Printed Crop Top', price:1000 },
-        { img: 'newin/nt12.avif', name: 'Pink Geometric Printed Crop Top', price: 1000}
+        { img: 'nt1.jpg', name: 'Brown-peach colored floral Gorgette crop top', price: 799 },
+        { img: 'nt2.jpg', name: 'Knitted white crop top', price: 1299},
+        { img: 'nt3.avif', name: 'Black Polyster crop top', price: 1000},
+        { img: 'nt4.avif', name: 'Short ruffle sleeved red crop top', price: 900 },
+        { img: 'nt5.avif', name: 'Long sleeved off-shoulder floral top', price: 800},
+        { img: 'nt6.avif', name: 'Long sleeved black top', price: 600 },
+        { img: 'nt7.avif', name: 'Pink Striped Bell Sleeves Crop Top', price:700 },
+        { img: 'nt8.avif', name: '>Womens Black Tie-Up Waist Crop Top', price:  800},
+        { img: 'nt9.avif', name: 'White Twist Detail Collar Neck top', price: 900},
+        { img: 'nt10.avif', name: 'Multi-Color Floral Print off-Shoulder Bardot Top', price:1000 },
+        { img: 'nt11.avif', name: 'Pink Geometric Printed Crop Top', price:1000 },
+        { img: 'nt12.avif', name: 'Pink Geometric Printed Crop Top', price: 1000}
     ];
 
     const productGrid = document.getElementById('productGrid');
