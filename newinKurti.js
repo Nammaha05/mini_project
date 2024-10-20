36c599d9ec426986c87cab6d356171b8b7801a1d
AthashreeDoiphode <athashreedoiphode@gmail.com>
2024-10-20 13:29:44 +0530

    Update newinKurti.js

--- newinKurti.js
+++ newinKurti.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
-        { img: 'newin/nk1.avif', name: 'Peach Colored Chikankaari Short Kurti', price: 800 },
-        { img: 'newin/nk2.avif', name: 'Peach Colored Chikankaari Short Kurti', price: 1000 },
-        { img: 'newin/nk3.avif', name: 'Red Embroidered Kurti', price: 1000 },
-        { img: 'newin/nk4.avif', name: 'Pink Chikankaari kurti', price: 1000 },
-        { img: 'newin/nk5.avif', name: 'White printed Kurti', price: 800 },
-        { img: 'newin/nk6.avif', name: 'Abstract Black Kurti', price: 800 },
-        { img: 'newin/nk7.avif', name: 'Crimson Embroidered Kurti', price: 800 },
-        { img: 'newin/nk8.avif', name: 'Light Pink Embroidered Kurti', price: 800 },
-        { img: 'newin/nk9.avif', name: 'Abstract Printed White Kurti', price: 800 },
-        { img: 'newin/nk10.avif', name: ' Pink Malai Silk Chikankaari Kurti', price: 800 },
-        { img: 'newin/nk11.avif', name: 'Black Printed Kurti', price: 800 },
-        { img: 'newin/nk12.avif', name: 'Navi Blue Embroidered Kurti', price: 800 }
+        { img: 'nk1.avif', name: 'Peach Colored Chikankaari Short Kurti', price: 800 },
+        { img: 'nk2.avif', name: 'Peach Colored Chikankaari Short Kurti', price: 1000 },
+        { img: 'nk3.avif', name: 'Red Embroidered Kurti', price: 1000 },
+        { img: 'nk4.avif', name: 'Pink Chikankaari kurti', price: 1000 },
+        { img: 'nk5.avif', name: 'White printed Kurti', price: 800 },
+        { img: 'nk6.avif', name: 'Abstract Black Kurti', price: 800 },
+        { img: 'nk7.avif', name: 'Crimson Embroidered Kurti', price: 800 },
+        { img: 'nk8.avif', name: 'Light Pink Embroidered Kurti', price: 800 },
+        { img: 'nk9.avif', name: 'Abstract Printed White Kurti', price: 800 },
+        { img: 'nk10.avif', name: ' Pink Malai Silk Chikankaari Kurti', price: 800 },
+        { img: 'nk11.avif', name: 'Black Printed Kurti', price: 800 },
+        { img: 'nk12.avif', name: 'Navi Blue Embroidered Kurti', price: 800 }
     ];
 
     const productGrid = document.getElementById('productGrid');
