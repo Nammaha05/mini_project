f1959b392af55f4062de6d7d67a67ee88ecdfebb
Nammaha05 <namratamahajan05@gmail.com>
2024-10-20 18:15:53 +0530

    Update mainmen.js

--- mainmen.js
+++ mainmen.js
@@ -112,3 +112,24 @@ document.addEventListener('DOMContentLoaded', () => {
         displayProducts(suitsets);
     }
 });
+
+// Open size chart modal
+const sizeChartBtn = document.getElementById("sizeChartBtn");
+const sizeChartModal = document.getElementById("sizeChartModal");
+const closeModal = document.getElementById("closeModal");
+
+sizeChartBtn.onclick = function () {
+    sizeChartModal.style.display = "block";
+}
+
+// Close size chart modal
+closeModal.onclick = function () {
+    sizeChartModal.style.display = "none";
+}
+
+// Close modal if user clicks outside of the modal
+window.onclick = function (event) {
+    if (event.target == sizeChartModal) {
+        sizeChartModal.style.display = "none";
+    }
+}
